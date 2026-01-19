4bda8ce733e4c4769fcdaeab019b88a8dbc385d3
Shahar Moshe Refaeli <44533369+ShaharM7@users.noreply.github.com>
2026-01-19 13:58:32 +0200

    inialzie setup - adding basic fixture for testing - env, clien, and autontication

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,45 +1,64 @@
+import logging
 import os
+from typing import Generator
 
 import pytest
-from typing import Generator
-from src.base_client import BaseClient
+from dotenv import load_dotenv
+
+from src.clients.base_client import BaseClient
+from src.models.auth.auth_models import AuthRequst, AuthResponse
+
+@pytest.fixture(scope="session", autouse=True)
+def loading_environments_variables() -> None:
+    """
+    Fixture that initializes the environments variables
+    """
+    if not load_dotenv(".env"):
+        raise FileNotFoundError(".env file missing! Please check your .env file")
+
+    _REQUIRED_ENV = [
+        "BASE_URL",
+        "ADMIN_USERNAME",
+        "ADMIN_PASSWORD",
+    ]
+
+    missing_env: [str] = []
+    for env in _REQUIRED_ENV:
+        if env not in os.environ:
+            missing_env.append(env)
+
+    if missing_env:
+        raise EnvironmentError(f"CRITICAL: Missing environments_variables: {', '.join(missing_env)}")
+        
+    logging.info("Initialized Loading Environments Variables fixture")
 
 
 @pytest.fixture(scope="session")
-def client() -> Generator[BaseClient, None, None]:
+def client(loading_environments_variables: None) -> Generator[BaseClient, None, None]:
     """
-    Fixture that initializes the BaseClient.
+    Fixture that initializes once the BaseClient.
     """
     api_client = BaseClient()
+    logging.info("Initialized BaseClient fixture")
     yield api_client
 
 
 @pytest.fixture(scope="session", autouse=True)
 def authenticate(client: BaseClient):
     """
-    Performs login and injects the token into the existing client.
+    Performs login once and injects the token into the existing BaseClient.
     """
 
-    username = os.getenv("API_PASSWORD")
-    password = os.getenv("API_PASSWORD")
+    auth_requst = AuthRequst(
+        username=os.environ["ADMIN_USERNAME"],
+        password=os.environ["ADMIN_PASSWORD"]
+    )
 
-    if not username or not password:
-        raise EnvironmentError("Please set API_PASSWORD and API_PASSWORD environment variables")
-
-    payload = {
-        "username": username,
-        "password": password,
-    }
-
-    response = client.post("/auth", payload)
+    response = client.post("/auth", auth_requst.model_dump())
     assert response.status_code == 200, "Authentication failed"
 
-    # המרת התשובה
-    auth_data = response.json()
+    auth_response: AuthResponse = AuthResponse(**response.json())
 
-    # בדיקת טיפוס (כמו שביקשת להשאיר)
-    assert isinstance(auth_data, dict), "Response format invalid: expected a dictionary"
-
-    # חילוץ הטוקן והזרקה ל-Headers
-    token = auth_data.get("token")
-    client.headers.update({"Cookie": f"token={token}"})
+    client.headers.update({"Authorization": auth_response.token})
+    logging.info("Authentication successful. Token Passed to Request Headers")
+    
